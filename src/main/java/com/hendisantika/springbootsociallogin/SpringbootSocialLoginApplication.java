88c019183dd8b687f65e4ada2077ebff521df0b8
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-12 06:56:50 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootsociallogin/SpringbootSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsociallogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSocialLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSocialLoginApplication.class, args);
+    }
+
+}
