a2dfc62d05186c88690db93235ffd3988ebb4931
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-12 07:07:59 +0700

    #14 - Add propertySourcesPlaceholderConfigurer in WebConfig

--- src/main/java/com/hendisantika/springbootsociallogin/config/WebConfig.java
+++ src/main/java/com/hendisantika/springbootsociallogin/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.hendisantika.springbootsociallogin.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,4 +18,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
