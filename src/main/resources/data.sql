33eef6c517816489bfc64e560078fd4d811a22f2
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-12 07:12:46 +0700

    #18 - Add data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO User (id, username, password)
+VALUES (1, 'naruto', 'naruto'),
+VALUES (2, 'sasuke', 'sasuke'),
+VALUES (3, 'sakura', 'sakura'),
+VALUES (4, 'minato', 'minato'),
+VALUES (5, 'kakashi', 'kakashi');
