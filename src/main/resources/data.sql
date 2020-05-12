34dfe5541e9e10e49814eb3b58ef99131a6fdc88
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-12 07:17:53 +0700

    #21 - Bug Fix SQL file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO User (id, username, password)
 VALUES (1, 'naruto', 'naruto'),
-VALUES (2, 'sasuke', 'sasuke'),
-VALUES (3, 'sakura', 'sakura'),
-VALUES (4, 'minato', 'minato'),
-VALUES (5, 'kakashi', 'kakashi');
+       (2, 'sasuke', 'sasuke'),
+       (3, 'sakura', 'sakura'),
+       (4, 'minato', 'minato'),
+       (5, 'kakashi', 'kakashi');
