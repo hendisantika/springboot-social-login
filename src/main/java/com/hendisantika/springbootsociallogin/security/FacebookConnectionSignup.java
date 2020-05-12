75f1ed209d21401489e72ed78a247356876ac406
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-12 07:03:56 +0700

    #7 - Add execute method in FacebookConnectionSignup

--- src/main/java/com/hendisantika/springbootsociallogin/security/FacebookConnectionSignup.java
+++ src/main/java/com/hendisantika/springbootsociallogin/security/FacebookConnectionSignup.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springbootsociallogin.security;
 
+import com.hendisantika.springbootsociallogin.model.User;
 import com.hendisantika.springbootsociallogin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionSignUp;
 import org.springframework.stereotype.Service;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +23,14 @@ public class FacebookConnectionSignup implements ConnectionSignUp {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Override
+    public String execute(Connection<?> connection) {
+        System.out.println("signup === ");
+        final User user = new User();
+        user.setUsername(connection.getDisplayName());
+        user.setPassword(randomAlphabetic(8));
+        userRepository.save(user);
+        return user.getUsername();
+    }
 }
