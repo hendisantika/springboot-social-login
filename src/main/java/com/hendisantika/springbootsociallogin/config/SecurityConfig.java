27c19c1d6a6f04f9da135a1d37c8bc80ba592a8f
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-12 07:04:44 +0700

    #9 - Add configure method in SecurityConfig

--- src/main/java/com/hendisantika/springbootsociallogin/config/SecurityConfig.java
+++ src/main/java/com/hendisantika/springbootsociallogin/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,4 +37,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private FacebookConnectionSignup facebookConnectionSignup;
 
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login*", "/signin/**", "/signup/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout();
+    } // @formatter:on
 }
