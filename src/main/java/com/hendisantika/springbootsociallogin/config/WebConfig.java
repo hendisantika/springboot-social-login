440ff2c6e2f27969ac3cea8632c4d5e119e99c1f
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-12 07:08:16 +0700

    #15 - Add configureDefaultServletHandling in WebConfig

--- src/main/java/com/hendisantika/springbootsociallogin/config/WebConfig.java
+++ src/main/java/com/hendisantika/springbootsociallogin/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.hendisantika.springbootsociallogin.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,5 +22,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Override
+    public void configureDefaultServletHandling(final DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
